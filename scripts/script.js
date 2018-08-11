3959ef9700c0cb735ef2396c2be8cb94a7556a2d
Esotastic <chrishjimenez@gmail.com>
2018-08-11 14:19:35 -0400

    Added diploma icon; created scripts page

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+//credit goes to Steven Frank of Cloud to Butt (https://github.com/panicsteve/cloud-to-butt/)
+
+walk(document.body);
+
+function walk(node)  
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1: 
+		case 9:  
+		case 11: 
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling; 
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: 
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bschool of hard knocks\b/g, "I'm a complete waste.");
+	v = v.replace(/\bhogwarts school of witchcraft and wizardry\b/g, "I'm a complete waste and a fukken nerd.");
+
+	textNode.nodeValue = v;
+}
